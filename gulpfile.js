61835c7952c3960c600392058a0bf37a53299e58
ug02fast <arthurzhuk@gmail.com>
2015-11-28 11:56:58 -0800

    idk anymore

--- gulpfile.js
+++ gulpfile.js
@@ -72,6 +72,14 @@ gulp.task('serve', function(done) {
     }));
 });
 
+gulp.task('serveprod', function() {
+  connect.server({
+    root: './',
+    port: process.env.PORT || 5200, // localhost:5000
+    livereload: false
+  });
+});
+
 gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -79,7 +87,7 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['build', 'serve', 'sass', 'watch']);
+gulp.task('default', ['build', 'serve', 'serveprod', 'sass', 'watch']);
 
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
